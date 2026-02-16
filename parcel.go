968e5785228866f1ad3f232e59ee1c7d9e322880
Dmitry Tkach <dmitkach@icloud.com>
2026-02-16 22:01:53 +0300

    - func (s ParcelStore) Delete(number int) is done

--- parcel.go
+++ parcel.go
@@ -114,8 +114,19 @@ func (s ParcelStore) SetAddress(number int, address string) error {
 }
 
 func (s ParcelStore) Delete(number int) error {
-	// реализуйте удаление строки из таблицы parcel
-	// удалять строку можно только если значение статуса registered
+	parcel, err := s.Get(number)
+	if err != nil {
+		return err
+	}
+
+	if parcel.Status != ParcelStatusRegistered {
+		return errors.New("status can be changed only for registered parcel")
+	}
+
+	_, err = s.db.Exec("DELETE FROM parcel WHERE number = :number", sql.Named("number", number))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
